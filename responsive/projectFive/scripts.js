4e229d053ecb0b4b064e617f98a3da9d46156e2d
petrockwd <petrockwd@gmail.com>
2018-10-15 10:04:05 -0400

    jquery start

---
+++ responsive/projectFive/scripts.js
@@ -0,0 +1,42 @@
+/*function paintingOn() {
+    document.getElementById('painting1').style.background='red';
+    document.getElementById('painting2').style.background='red';
+    document.getElementById('painting3').style.background='red';
+    document.getElementById('painting4').style.background='red';
+  }
+
+  function paintingOff() {
+    document.getElementById('painting1').style.background='inherit';
+    document.getElementById('painting2').style.background='inherit';
+    document.getElementById('painting3').style.background='inherit';
+    document.getElementById('painting4').style.background='inherit';
+  }
+
+  function hennaOn() {
+    document.getElementById('henna1').style.background='red';
+    document.getElementById('henna2').style.background='red';
+    document.getElementById('henna3').style.background='red';
+    document.getElementById('henna4').style.background='red';
+  }
+
+  function hennaOff() {
+    document.getElementById('henna1').style.background='inherit';
+    document.getElementById('henna2').style.background='inherit';
+    document.getElementById('henna3').style.background='inherit';
+    document.getElementById('henna4').style.background='inherit';
+  }
+
+  function caligraphyOn() {
+    document.getElementById('caligraphy1').style.background='red';
+    document.getElementById('caligraphy2').style.background='red';
+    document.getElementById('caligraphy3').style.background='red';
+    document.getElementById('caligraphy4').style.background='red';
+  }
+
+  function caligraphyOff() {
+    document.getElementById('caligraphy1').style.background='inherit';
+    document.getElementById('caligraphy2').style.background='inherit';
+    document.getElementById('caligraphy3').style.background='inherit';
+    document.getElementById('caligraphy4').style.background='inherit';
+  }
+*/
